812f807f1cb96a5e1190be0840e42873d3019d28
ArseniyKhal <asmn@mail.ru>
2023-08-28 15:14:47 +0500

    add font from js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import './App.css'
 import { useEffect, useState } from 'react'
 import NavMenu from './components/NavMenu/NavMenu'
 import Sidebar from './components/Sidebar/Sidebar'
--- src/App.styles.js
+++ src/App.styles.js
@@ -2,22 +2,20 @@ import { styled, createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
 
-// не понятно как подключить шрифт?
 @font-face {
 	font-family: "StratosSkyeng";
 	src: local("StratosSkyeng"), local("StratosSkyeng"),
-		url("../public/fonts/StratosSkyeng.woff2") format("woff2"),
-		url("../public/fonts/StratosSkyeng.woff") format("woff");
+		url("/fonts/StratosSkyeng.woff2") format("woff2"),
+		url("/fonts/StratosSkyeng.woff") format("woff");
 	font-weight: 400;
 	font-style: normal;
 }
-
 * {
 	margin: 0;
 	padding: 0;
 	-webkit-box-sizing: border-box;
 	box-sizing: border-box;
-}
+	}
 *:before,
 *:after {
 	-webkit-box-sizing: border-box;
@@ -39,7 +37,6 @@ button,
 	cursor: pointer;
 }
 html, body {
-	
 	width: 100%;
 	height: 100%;
 	font-family: "StratosSkyeng", sans-serif;
